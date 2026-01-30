41fcaac26f1c67db53a3c3e9cd3bdcf3ab71e85a
Antoine Grenier <AGrenier@CegepGranby.qc.ca>
2026-01-29 21:46:50 -0500

    Ajout la solution pour exercices cours semaine 2A - MVC

---
+++ S2-A-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace S2_A_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
