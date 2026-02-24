ead130e27a8a859ac203c61cf1af6d0d385248f9
Antoine Grenier <agrenier@cegepgranby.qc.ca>
2026-02-24 12:31:20 -0500

    Nettoyage cours 4-B-EF

---
+++ S4-B-EF/S4-B-EF.Seeder/DbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using S4_B_EF.Data;
+
+namespace S4_B_EF.Seeder
+{
+    public class DbContextFactory
+    {
+        public static ApplicationDbContext CreateDbContext()
+        {
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(@Directory.GetCurrentDirectory() + "/appsettings.json")
+            .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("Exercice3_ModeleEFCoreDBContext");
+            builder.UseSqlServer(connectionString);
+            return new ApplicationDbContext(builder.Options);
+        }
+
+    }
+}
---
+++ S4-B-EF/S4-B-EF.Seeder/Program.cs
@@ -0,0 +1,38 @@
+﻿using Bogus;
+using S4_B_EF.Models;
+using S4_B_EF.Seeder;
+
+public partial class Program
+{
+    private static void Main(string[] args)
+    {
+        using var context = DbContextFactory.CreateDbContext();
+
+
+        // --- Faker Pays ---
+        var paysFaker = new Faker<Pays>("fr")
+            .RuleFor(x => x.Id, 0) // laisser EF auto-générer
+            .RuleFor(x => x.Nom, f => f.Address.Country());
+
+        // --- Faker Département ---
+        var departementFaker = new Faker<Departement>("fr")
+            .RuleFor(x => x.Id, 0)
+            .RuleFor(x => x.Nom, f =>
+            {
+                var name = f.Commerce.Department();
+                // respecter [MaxLength(50)] si présent
+                return name.Length > 50 ? name.Substring(0, 50) : name;
+            })
+            .RuleFor(x => x.BudgetAnnuel, f => Math.Round(f.Random.Double(200_000, 5_000_000), 2))
+            .RuleFor(x => x.DirecteurId, _ => null)  // sera fixé après création des employés
+            .RuleFor(x => x.Directeur, _ => null);
+
+        // --- Générer Pays et Départements d'abord ---
+        var pays = paysFaker.Generate(10);
+        var departements = departementFaker.Generate(6);
+
+        context.AddRange(pays);
+        context.AddRange(departements);
+        context.SaveChanges();
+    }
+}
